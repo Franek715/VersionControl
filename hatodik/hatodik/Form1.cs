2cc72d199ea02517be70d2d71e2a7fe2b83bf9b4
Franek715 <francsicsd@freemail.hu>
2020-10-18 21:52:09 +0200

    commit

---
+++ hatodik/hatodik/Form1.cs
@@ -0,0 +1,43 @@
+﻿using hatodik.MnbServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hatodik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public String fuggveny()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+            return result;
+        }
+    }
+}
