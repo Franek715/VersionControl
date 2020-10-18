a38fb8793b659879f863d86aa6b1b39646857d78
Franek715 <francsicsd@freemail.hu>
2020-10-18 22:21:37 +0200

    utso

--- hatodik/hatodik/Form1.cs
+++ hatodik/hatodik/Form1.cs
@@ -1,4 +1,5 @@
-﻿using hatodik.MnbServiceReference;
+﻿using hatodik.Entities;
+using hatodik.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace hatodik
 {
@@ -16,7 +19,25 @@ namespace hatodik
         public Form1()
         {
             InitializeComponent();
+            dgw1.DataSource = Rates;
+            chart1.DataSource = Rates;
+
+            var series = chart1.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chart1.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chart1.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
+
+        BindingList<RateData> Rates = new BindingList<RateData>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -38,6 +59,32 @@ namespace hatodik
 
             var result = response.GetExchangeRatesResult;
             return result;
+        }
+
+        public RateData fgv()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(fuggveny());
+
+            var rate = new RateData();
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                Rates.Add(rate);
+
+                 rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+
+            return rate;
+
         }
     }
 }
