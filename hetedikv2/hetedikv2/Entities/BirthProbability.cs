d6bad1c8c48bdc121e7ad88acb12707eb63c0427
Franek715 <francsicsd@freemail.hu>
2020-10-25 22:09:49 +0100

    cmmt

---
+++ hetedikv2/hetedikv2/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hetedikv2.Entities
+{
+    class BirthProbability
+    {
+        public int age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double p { get; set; }
+    }
+}
---
+++ hetedikv2/hetedikv2/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hetedikv2.Entities
+{
+    class DeathProbability
+    {
+        public int age { get; set; }
+        public Gender Gender { get; set; }
+        public double p { get; set; }
+    }
+}
